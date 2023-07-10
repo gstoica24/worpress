3dfa9bb6364c0cfae83e62975b6282e6045ae242
Stoica Gabriel <gstoica11@gmail.com>
2023-07-10 16:49:05 +0200

    ajoute plus de style etc.. + solution des quelque probleme

--- category.php
+++ category.php
@@ -2,22 +2,24 @@
 
 get_header();
 
-echo "<div>";
-echo "<h1> Categorie </h1>";
+echo "<div class='category'>";
+echo "<h1>";
+echo  single_cat_title();
+echo "</h1>";
 echo "</div>";
 
-echo "<section>";
+echo "<section class='category-content'>";
 while (have_posts()) :
     the_post();
     echo "<a href='" . get_permalink() . "'>";
-    echo "<article>";
+    echo "<article class='category-post'>";
     if (has_post_thumbnail()) {
         $thumbnail_id = get_post_thumbnail_id();
         $thumbnail_url = wp_get_attachment_image_src($thumbnail_id, 'medium')[0];
         echo '<img src="' . $thumbnail_url . '" alt="' . get_the_title() . '" />';
     }
-    the_title();
-    the_content();
+    the_title("<h4>", "</h4>");
+    the_content("<p>", "</p>");
     echo "</article>";
     echo "</a>";
 endwhile;
--- index.php
+++ index.php
@@ -5,8 +5,12 @@ get_header();
 ?>
 
 <section class="header">
-    <h1>Titre</h1>
-    <h3>Sous titre</h3>
+    <h1>L'Association pour la Défense de l'Environnement à Grenoble (ADEG)</h1>
+    <h3>"Agissons ensemble pour un avenir durable !”</h3>
+    <p>L'Association pour la Défense de l'Environnement à Grenoble (ADEG) est un collectif
+        engagé qui œuvre à la préservation de l'environnement local,
+        en sensibilisant la population, en protégeant les écosystèmes et en plaidant pour des politiques durables,
+        afin de construire un avenir harmonieux pour notre région.</p>
 </section>
 <h4>Nos actus</h4>
 <section class="content">
@@ -15,7 +19,7 @@ get_header();
         the_post();
         echo "<a href='" . get_permalink() . "'>";
         echo "<article class='preview'>";
-        the_title();
+        the_title("<h4>", "</h4>");
         if (has_post_thumbnail()) {
             $thumbnail_id = get_post_thumbnail_id();
             $thumbnail_url = wp_get_attachment_image_src($thumbnail_id, 'medium')[0];
