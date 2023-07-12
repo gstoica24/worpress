ef5d83e62a03ad26f8e112b7a29467b45b33392e
Stoica Gabriel <gstoica11@gmail.com>
2023-07-12 07:51:14 +0200

    more and more style

--- index.php
+++ index.php
@@ -12,21 +12,22 @@ get_header();
         en sensibilisant la population, en protégeant les écosystèmes et en plaidant pour des politiques durables,
         afin de construire un avenir harmonieux pour notre région.</p>
 </section>
-<h4>Nos actus</h4>
+<h4 class="actualite">Nos actus</h4>
 <section class="content">
     <?php
     while (have_posts()) :
         the_post();
-        echo "<a href='" . get_permalink() . "'>";
+
         echo "<article class='preview'>";
         the_title("<h4>", "</h4>");
         if (has_post_thumbnail()) {
             $thumbnail_id = get_post_thumbnail_id();
             $thumbnail_url = wp_get_attachment_image_src($thumbnail_id, 'medium')[0];
+            echo "<a href='" . get_permalink() . "'>";
             echo '<img src="' . $thumbnail_url . '" alt="' . get_the_title() . '" />';
+            echo "</a>";
         }
         echo "</article>";
-        echo "</a>";
     endwhile;
     ?>
 </section>
